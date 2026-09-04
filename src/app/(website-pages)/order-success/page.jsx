d74d7be60c5204889c7dd004dad36c16343856d1
only-abhay <abhayshaw6729@gmail.com>
2026-09-04 14:53:43 +0530

    Fix order success build

--- src/app/(website-pages)/order-success/page.jsx
+++ src/app/(website-pages)/order-success/page.jsx
@@ -1,4 +1,4 @@
-import OrderSuccessContent from "./OrderSuccessContent";
+import OrderSuccessContent from "@/components/websiteComponent/OrderSuccess/OrderSuccess";
 
 export default async function OrderSuccessPage({ searchParams }) {
   const params = await searchParams;
